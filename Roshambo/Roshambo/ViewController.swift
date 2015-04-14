a441157e189f026b69e22ca9f88063d7a9e3780b
umarqattan <umarqattan@ucla.edu>
2015-04-14 11:02:14 -0700

    Add Roshambo's skeleton

---
+++ Roshambo/Roshambo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Umar Qattan on 4/13/15.
+//  Copyright (c) 2015 Umar Qattan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func rock()
+    {
+        /**
+        TODO:    Simulate a rock/paper/scissors game by presenting an image of
+                 the opponent's move (if you pick rock and the opponent picks
+                 paper, a picture of a rock covered with a piece of paper shou-
+                 present itself. A label under the image will read somerthing
+                 along the lines of "Paper covers rock. You lose." Afterward, 
+                 you should have the option to play again, which should take the
+                 presented view back to the originally presenting view.
+        **/
+        
+        
+        
+        
+    }
+   
+    
+
+}
+
---
+++ Roshambo/RoshamboViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RoshamboViewController.swift
+//  Roshambo
+//
+//  Created by Umar Qattan on 4/14/15.
+//  Copyright (c) 2015 Umar Qattan. All rights reserved.
+//
+
+import UIKit
+
+
+class RoshamboViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+}
